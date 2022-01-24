15eb1d1f471ba8a0addb6c7c85289ec512c9c43f
CohenArthur <arthur.cohen@epita.fr>
2022-01-24 18:37:27 +0100

    bma: Add more functionality to library

--- bma220.c
+++ bma220.c
@@ -2,46 +2,119 @@
 
 #define BMA220_ADDR 0x0A
 
-#define CHIPID_REG 0x00
-#define SOFTRESET_REG 0x32
+#define REG_CHIPID 0x00
+#define REG_SOFTRESET 0x32
+#define REG_AXIS_X 0x04
+#define REG_AXIS_Y 0x06
+#define REG_AXIS_Z 0x08
+#define REG_RANGE 0x22
 
-static int read_reg(struct bma220_dev *dev, uint8_t reg, uint8_t *value)
-{
-	if (dev->write(BMA220_ADDR, &reg, 1))
-		return -1;
+static int read_reg(struct bma220_dev *dev, uint8_t reg, uint8_t *value) {
+  if (dev->write(BMA220_ADDR, &reg, 1))
+    return -1;
 
-	if (dev->read(BMA220_ADDR, value, 1))
-		return -1;
+  if (dev->read(BMA220_ADDR, value, 1))
+    return -1;
 
-	return 0;
+  return 0;
 }
 
-int bma220_init(struct bma220_dev *dev)
-{
-	if (!dev->write || !dev->read)
-		return BMA220_NULL_FUNC;
+static int write_reg(struct bma220_dev *dev, uint8_t reg, uint8_t value) {
+  uint8_t data[2] = {reg, value};
 
-	uint8_t reset_value;
-	if (read_reg(dev, SOFTRESET_REG, &reset_value))
-		return BMA220_COMMUNICATION;
+  if (dev->write(BMA220_ADDR, data, 2))
+    return -1;
 
-	if (reset_value == 0xFF)
-		if (read_reg(dev, SOFTRESET_REG, &reset_value))
-			return BMA220_COMMUNICATION;
-
-	if (reset_value != 0x00)
-		return BMA220_INVALID_MODE;
-
-	if (read_reg(dev, SOFTRESET_REG, &reset_value))
-		return BMA220_COMMUNICATION;
-
-	if (reset_value != 0xFF)
-		return BMA220_INVALID_MODE;
-
-	return 0;
+  return 0;
 }
 
-int bma220_chip_id(struct bma220_dev *dev, uint8_t *chip_id)
-{
-	return read_reg(dev, CHIPID_REG, chip_id);
+int bma220_init(struct bma220_dev *dev) {
+  if (!dev->write || !dev->read)
+    return BMA220_NULL_FUNC;
+
+  uint8_t reset_value;
+  if (read_reg(dev, REG_SOFTRESET, &reset_value))
+    return BMA220_COMMUNICATION;
+
+  if (reset_value == 0xFF)
+    if (read_reg(dev, REG_SOFTRESET, &reset_value))
+      return BMA220_COMMUNICATION;
+
+  if (reset_value != 0x00)
+    return BMA220_INVALID_MODE;
+
+  if (read_reg(dev, REG_SOFTRESET, &reset_value))
+    return BMA220_COMMUNICATION;
+
+  if (reset_value != 0xFF)
+    return BMA220_INVALID_MODE;
+
+  return 0;
+}
+
+int bma220_chip_id(struct bma220_dev *dev, uint8_t *chip_id) {
+  return read_reg(dev, REG_CHIPID, chip_id);
+}
+
+int bma220_set_range_mode(struct bma220_dev *dev, uint8_t mode) {
+  return write_reg(dev, REG_RANGE, mode);
+}
+
+int bma220_get_range_mode(struct bma220_dev *dev, uint8_t *mode) {
+  int res = read_reg(dev, REG_RANGE, mode);
+
+  return res;
+}
+
+static float low_level_conversion(int8_t value, uint8_t range_mode) {
+  // We now need to scale the byte value to its actual floating point value.
+  // The scale is equal to the following: 32 / (2 ^ (range_mode + 1)).
+  // For example, RANGE_2G is equal to 0, so range_mode + 1 equals 1.
+  // 32 / 2 ^ 1 => 32 / 2 => 16 which is the proper scale to get a range of
+  // 64 values between +2 and -2g
+  // Likewise, RANGE_2G = 3, so range_mode + 1 equals 4
+  // 32 / 2 ^ 4 => 32 / 16 => 2 which corresponds to value between +16 and -16g
+
+  uint32_t scale = (1 << (range_mode + 1));
+  scale = 32.0 / scale;
+
+  return ((float)value) / ((float)scale);
+}
+
+static int convert_value(struct bma220_dev *dev, int8_t value, float *result) {
+  uint8_t range_mode;
+  int ret = bma220_get_range_mode(dev, &range_mode);
+  if (ret)
+    return ret;
+
+  *result = low_level_conversion(value, range_mode);
+
+  return 0;
+}
+
+static int8_t convert_acc_register(uint8_t value) { return (int8_t)value / 4; }
+
+int bma220_get_acceleration(struct bma220_dev *dev,
+                            struct bma220_acceleration_data *data) {
+  uint8_t raw_x, raw_y, raw_z;
+  int8_t x, y, z;
+
+  if (read_reg(dev, REG_AXIS_X, &raw_x))
+    return BMA220_COMMUNICATION;
+
+  if (read_reg(dev, REG_AXIS_Y, &raw_y))
+    return BMA220_COMMUNICATION;
+
+  if (read_reg(dev, REG_AXIS_Z, &raw_z))
+    return BMA220_COMMUNICATION;
+
+  x = convert_acc_register(raw_x);
+  y = convert_acc_register(raw_y);
+  z = convert_acc_register(raw_z);
+
+  convert_value(dev, x, &data->x);
+  convert_value(dev, y, &data->y);
+  convert_value(dev, z, &data->z);
+
+  return 0;
 }
--- bma220.h
+++ bma220.h
@@ -3,23 +3,32 @@
 
 #include <stdint.h>
 
+/* Range values for bma220_set_range_mode() */
+#define RANGE_2G 0
+#define RANGE_4G 1
+#define RANGE_8G 2
+#define RANGE_16G 3
+
+#define BMA220_CHIP_ID 0xDD
+
 /**
  * Write function for the bma220_dev write field
  *
- * The given device address corresponds to the *raw* address of the device. In some
- * cases such as on the STM32, it is necessary for the write function to shift the
- * adresse before calling the low level write function.
+ * The given device address corresponds to the *raw* address of the device. In
+ * some cases such as on the STM32, it is necessary for the write function to
+ * shift the adresse before calling the low level write function.
  *
  * For example, HAL_I2C_Master_Transmit(i2c_handle, dev_addr << 1, data, len);
  */
-typedef int (*bma220_write_f)(uint8_t dev_addr, const uint8_t *data, uint32_t len);
+typedef int (*bma220_write_f)(uint8_t dev_addr, const uint8_t *data,
+                              uint32_t len);
 
 /**
  * Read function for the bma220_dev read field
  *
- * The given device address corresponds to the *raw* address of the device. In some
- * cases such as on the STM32, it is necessary for the write function to shift the
- * adresse before calling the low level write function.
+ * The given device address corresponds to the *raw* address of the device. In
+ * some cases such as on the STM32, it is necessary for the write function to
+ * shift the adresse before calling the low level write function.
  *
  * For example, HAL_I2C_Master_Receive(i2c_handle, dev_addr << 1, data, len);
  */
@@ -29,20 +38,20 @@ typedef int (*bma220_read_f)(uint8_t dev_addr, uint8_t *data, uint32_t len);
  * BMA220 handler structure
  */
 struct bma220_dev {
-    /* Function to use to read from the device */
-    bma220_read_f read;
-    /* Function to use to write to the device */
-    bma220_write_f write;
+  /* Function to use to read from the device */
+  bma220_read_f read;
+  /* Function to use to write to the device */
+  bma220_write_f write;
 };
 
 /**
  * Possible return values from BMA220 functions including error codes
  */
 enum bma220_retval {
-    BMA220_OK = 0,
-    BMA220_NULL_FUNC,
-    BMA220_COMMUNICATION,
-    BMA220_INVALID_MODE,
+  BMA220_OK = 0,
+  BMA220_NULL_FUNC,
+  BMA220_COMMUNICATION,
+  BMA220_INVALID_MODE,
 };
 
 /**
@@ -57,14 +66,48 @@ enum bma220_retval {
 int bma220_init(struct bma220_dev *dev);
 
 /**
- * Fetch the acceleration value from a device
+ * Set the range mode to use before fetching acceleration values.
+ * By default, the device is in 2g range mode.
  *
- * @dev Pointer of the device to read
- * @acceleration Pointer in which to store the result
+ * @dev Pointer to the device to set
+ * @mode Value to set. The value *must* be one of RANGE_2G, RANGE_4G, RANGE_8G
+ * or RANGE_16G
  *
  * @return 0 on success, an error code otherwise
  */
-int bma220_acceleration(struct bma220_dev *dev, int8_t *acceleration);
+int bma220_set_range_mode(struct bma220_dev *dev, uint8_t mode);
+
+/**
+ * Get the range mode currently set in the device
+ *
+ * @dev Pointer to the device to read from
+ * @mode Pointer to the mode to set
+ *
+ * @return 0 on success, an error code otherwise
+ */
+int bma220_get_range_mode(struct bma220_dev *dev, uint8_t *mode);
+
+/**
+ * Acceleration data returned by the bma220_acceleration() function.
+ *
+ * This structure wraps the three axes that the sensor can handle
+ */
+struct bma220_acceleration_data {
+  float x;
+  float y;
+  float z;
+};
+
+/**
+ * Fetch the acceleration value from a device
+ *
+ * @dev Pointer of the device to read
+ * @data Pointer in which to store the acceleration result
+ *
+ * @return 0 on success, an error code otherwise
+ */
+int bma220_get_acceleration(struct bma220_dev *dev,
+                            struct bma220_acceleration_data *data);
 
 /**
  * Fetch the chip ID of a device
